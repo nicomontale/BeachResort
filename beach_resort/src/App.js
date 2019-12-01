d84d369e1a14bfec3f53ba3d8345be67bda791ea
nicomontale <roxas.nm@gmail.com>
2019-12-01 11:48:37 +0100

    Navbar

--- beach_resort/src/App.js
+++ beach_resort/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 //import './App.css';
 import home from './pages/home';
 import rooms from './pages/rooms';
 import singleroom from './pages/singleroom';
+import {Route, Switch} from 'react-router-dom'; 
 function App() {
   return (
-    <>
-    
-    
-    </>
+    <div className="app">
+    <Switch>
+    <Route exact path="/" component={home}/> 
+    <Route exact path="/rooms" component={rooms}/> 
+    <Route exact path="/rooms:slug" component={singleroom}/> 
+    <Route component={Error}  />
+    </Switch>
+    </div>
   );
 }
 
